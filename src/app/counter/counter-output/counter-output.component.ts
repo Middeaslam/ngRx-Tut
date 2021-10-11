8ed7bcd22281506006835bdb91d6663c6db8c3ca
Mahammad Aslam <middeaslam36@gmail.com>
2021-10-11 16:35:50 +0530

    Counter Operations using ngRx

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-counter-output',
@@ -6,12 +7,14 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./counter-output.component.css']
 })
 export class CounterOutputComponent implements OnInit {
+  counter!: number;
 
-  @Input() counter: any;
-
-  constructor() { }
+  constructor(private store: Store<{counter: {counter: number}}>) { }
 
   ngOnInit(): void {
+    this.store.select('counter').subscribe(data => {
+      this.counter = data.counter
+    })
   }
 
 }
